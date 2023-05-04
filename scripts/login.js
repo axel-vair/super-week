63023c3435037147976beacaeee517af2df3db59
axelvair <axel.vair@laplateforme.io>
2023-05-04 14:30:29 +0200

    add js file for listen form and fetch

---
+++ scripts/login.js
@@ -0,0 +1,20 @@
+let connectionForm = document.getElementById('connectionForm');
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+    let form = new FormData(event.currentTarget);
+    let url = "register";
+    let request = new Request(url, {method: 'POST', body: form});
+    let response = await fetch(request);
+    let responseData = await response.json();
+    if (responseData.success) {
+        let errorDiv = document.querySelector('.error');
+        errorDiv.innerHTML = "Connexion réussie";
+        window.location.replace('/users')
+    } else {
+        let errorDiv = document.querySelector('.error');
+        errorDiv.innerHTML = "La connexion a échoué";
+    }
+}
+
+registerForm.addEventListener('submit', (event) => handleFormSubmit(event));
